85ba8f62625dbdd1367a6e4fd20de67f66b725bf
albgv1402 <albertogarv1402@gmail.com>
2020-07-30 19:22:56 -0500

    Comments on GameScene and Bulles classes

--- src/Classes/Guns.js
+++ src/Classes/Guns.js
@@ -1,8 +1,23 @@
+/*
+    Clase Guns
+    Para guardar la información referente a las armas
+*/
 class Guns {
+    /*
+        Constructor
+        Entrada
+        - mesh: mesh de Three JS
+    */
     constructor(mesh) {
         this.mesh = mesh;
     }
 
+    /*
+        Se encarga de hacer que el jugador dispare
+        Entrada:
+        - raycaster:    raycaster con la dirección en la que el jugador está apuntando
+        - controls:     controles del jugador para conseguir la posición del jugador
+    */
     shoot(raycaster, controls) {
         createBullet(250, raycaster.ray.direction, controls.getObject(), 15, 'player');
     }
