4ee42e656c65892d25db93c997834938dd1a7214
albgv1402 <albertogarv1402@gmail.com>
2020-07-30 23:58:44 -0500

    More comments

--- src/Classes/Entity.js
+++ src/Classes/Entity.js
@@ -1,9 +1,19 @@
 class Entity {
+    
+    /*
+        Constructor
+        Entrada:
+        - mesh:         Mesh de ThreeJS de la entidad 
+        - cannonBody:   Cuerpo de CannonJS
+    */
     constructor(mesh, cannonBody) {
         this.mesh = mesh;
         this.cannonBody = cannonBody;
     }
 
+    /*
+        Actualiza la posición del mesh de acuerdo a su respectivo cuerpo de CannonJS
+    */
     updatePos() {
         this.mesh.position.copy(this.cannonBody.position);
     }
